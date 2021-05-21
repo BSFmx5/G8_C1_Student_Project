f60dd401aaeff914cbb3b267537bfddf4f3f637b
Pratham2415 <84369631+Pratham2415@users.noreply.github.com>
2021-05-21 19:33:50 +0530

    Create Student_Project.py

---
+++ Student_Project.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.penup()
+turtle.setposition(#write the location here)
+turtle.pendown()
+turtle.pencolor("red")
+turtle.circle(100)
+
+turtle.penup()
+turtle.setposition(#write the location here)
+turtle.pendown()
+turtle.pencolor("green")
+turtle.circle(200)
