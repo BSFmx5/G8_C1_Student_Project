053e236aae53205be8f7a1210c11639839c2df29
Pratham2415 <84369631+Pratham2415@users.noreply.github.com>
2021-05-21 21:07:39 +0530

    Update Student_Project.py

--- Student_Project.py
+++ Student_Project.py
@@ -3,11 +3,11 @@ import turtle
 turtle.penup()
 turtle.setposition(100,100)
 turtle.pendown()
-turtle.pencolor("red")
+turtle.pencolor()#write the color of pen
 turtle.circle(100)
 
 turtle.penup()
-turtle.setposition(#write the location here)
+turtle.setposition()#write the location here
 turtle.pendown()
-turtle.pencolor("green")
+turtle.pencolor()#write the color of pen
 turtle.circle(200)
